9d5ad40133a326ffd249af3e9a847333bcf34c02
georgelau <laugeorge@gmail.com>
2019-09-23 14:51:39 -0400

    updated friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'George',
+      profilePic: 'https://avatars3.githubusercontent.com/u/39841202?s=400&u=2a60909608563370762e467ee70e6d0449eadbd8&v=4',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
